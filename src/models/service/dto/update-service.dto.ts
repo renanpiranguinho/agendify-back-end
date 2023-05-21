2276e853bb95beaecb90496a1e392ba10fad2c45
Renan <renanpiranguinho@gmail.com>
2023-05-21 14:40:37 -0300

    refactor: fix price number type

--- src/models/service/dto/update-service.dto.ts
+++ src/models/service/dto/update-service.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateServiceDto } from './create-service.dto';
+import { IsNumber } from 'class-validator';
 
-export class UpdateServiceDto extends PartialType(CreateServiceDto) {}
+export class UpdateServiceDto extends PartialType(CreateServiceDto) {
+  @IsNumber()
+  price?: number;
+}
