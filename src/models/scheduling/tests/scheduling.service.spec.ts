07001e4f217a8292d392d70ed95db7b4f23c826e
Renan Vitor <renanpiranguinho@gmail.com>
2023-07-11 10:06:47 -0300

    fix: fix scheduling tests

--- src/models/scheduling/tests/scheduling.service.spec.ts
+++ src/models/scheduling/tests/scheduling.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SchedulingService } from '../scheduling.service';
+import { SchedulingRepository } from '../repository/scheduling.repository';
+import { ServicesRepository } from '../../service/repository/services.repository';
+import { PrismaService } from '../../../../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { AvailabilityRepository } from '../../availability/repository/availability.repository';
 
 describe('SchedulingService', () => {
   let service: SchedulingService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SchedulingService],
+      providers: [
+        SchedulingService,
+        ServicesRepository,
+        SchedulingRepository,
+        PrismaService,
+        AvailabilityRepository,
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn().mockReturnValue('token'),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<SchedulingService>(SchedulingService);
